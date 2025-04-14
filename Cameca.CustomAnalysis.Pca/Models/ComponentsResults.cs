ac46fdebfa8cee561bb626857d822ef515a2ae0e
Olof Hellman <olof@mac.com>
2025-04-14 22:43:01 +0100

    More changes brought over from olofhellman repo
    More changes brought over from olofhellman repo

--- Cameca.CustomAnalysis.Pca/Models/ComponentsResults.cs
+++ Cameca.CustomAnalysis.Pca/Models/ComponentsResults.cs
@@ -1,13 +1,17 @@
 ﻿using Cameca.CustomAnalysis.Interface;
+using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace Cameca.CustomAnalysis.Pca;
 
-internal sealed class ComponentsResults
+public sealed class ComponentsResults
 {
     public IGrid3DData Grid3DData { get; }
 
     public int[] VoxelIndices { get; }
+
+    public PhaseIdResults PhaseIDResults { get; set; }
 
     public List<ComponentResults> Components { get; }
 
@@ -16,6 +20,11 @@ internal sealed class ComponentsResults
         Grid3DData = grid3DData;
         VoxelIndices = voxelIndices;
         Components = components;
+
+        List<int> emptyList = new List<int>();
+        emptyList.Add(0);
+        emptyList[0] = 0;
+        PhaseIDResults = new PhaseIdResults(emptyList, 1);
     }
 
 }
