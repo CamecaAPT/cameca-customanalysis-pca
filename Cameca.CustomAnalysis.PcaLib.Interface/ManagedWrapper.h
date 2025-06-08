c495a9375d2c7a0182cd224ae2ae461ceae257b9
Jim Payne <jim.payne@ametek.com>
2025-06-07 20:42:23 -0500

    Add c++/cli interface project for native interop

---
+++ Cameca.CustomAnalysis.PcaLib.Interface/ManagedWrapper.h
@@ -0,0 +1,36 @@
+#pragma once
+
+namespace Cameca::CustomAnalysis::PcaLib::Interface {
+
+    template<class T>
+    public ref class ManagedWrapper abstract
+
+    {
+    public:
+        ManagedWrapper(T* instance)
+            : m_Instance(instance)
+        {
+        }
+        virtual ~ManagedWrapper()
+        {
+            if (m_Instance != nullptr)
+            {
+                delete m_Instance;
+            }
+        }
+        !ManagedWrapper()
+        {
+            if (m_Instance != nullptr)
+            {
+                delete m_Instance;
+            }
+        }
+    protected:
+        T* GetInstance()
+        {
+            return m_Instance;
+        }
+    private:
+        T* m_Instance;
+    };
+}
