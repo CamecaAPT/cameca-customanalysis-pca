d9beb8cc6ad4478b5700d8ea294cdacddf1c078d
Jim Payne <jim.payne@ametek.com>
2026-06-24 13:55:37 -0500

    Extract base conditional menu factory base

---
+++ Cameca.CustomAnalysis.Pca/Features/ConditionalMenuFactoryBase.cs
@@ -0,0 +1,49 @@
+﻿using Cameca.CustomAnalysis.Interface;
+using Prism.Commands;
+using Prism.Events;
+using System.Linq;
+
+namespace Cameca.CustomAnalysis.Pca;
+
+internal abstract class ConditionalMenuFactoryBase : IAnalysisMenuFactory
+{
+    private readonly IEventAggregator eventAggregator;
+    private readonly INodeInfoProvider nodeInfoProvider;
+
+    protected ConditionalMenuFactoryBase(IEventAggregator eventAggregator, INodeInfoProvider nodeInfoProvider)
+    {
+        this.eventAggregator = eventAggregator;
+        this.nodeInfoProvider = nodeInfoProvider;
+    }
+
+
+    public abstract AnalysisMenuLocation Location { get; }
+    protected abstract INodeDisplayInfo DisplayInfo { get; }
+    protected abstract string NodeUniqueId { get; }
+    public virtual string? ToolTip { get; }
+
+    public virtual string[]? AllowedTypes { get; } = null;
+    public virtual string[]? DisallowedTypes { get; } = null;
+
+    public IMenuItem CreateMenuItem(IAnalysisMenuContext context)
+    {
+        var nodeInfo = nodeInfoProvider.Resolve(context.NodeId);
+        if (nodeInfoProvider.Resolve(context.NodeId) is { TypeId: string nodeType } && ShowMenuItem(nodeType))
+        {
+            IAnalysisMenuContext context2 = context;
+            return new MenuAction(DisplayInfo.Title, new DelegateCommand(() =>
+            {
+                eventAggregator.PublishCreateNode(NodeUniqueId, context2.NodeId, DisplayInfo.Title, DisplayInfo.Icon);
+            }), DisplayInfo.Icon, true, ToolTip);
+        }
+        return null;
+    }
+
+    private bool ShowMenuItem(string nodeType)
+    {
+        return IsAllowed(nodeType) && !IsDisallowed(nodeType);
+
+        bool IsAllowed(string nodeId) => AllowedTypes is null || AllowedTypes.Contains(nodeId);
+        bool IsDisallowed(string nodeId) => DisallowedTypes is not null && DisallowedTypes.Contains(nodeId);
+    }
+}
--- Cameca.CustomAnalysis.Pca/Features/Voxelization/VoxelizationMenuFactory.cs
+++ Cameca.CustomAnalysis.Pca/Features/Voxelization/VoxelizationMenuFactory.cs
@@ -4,14 +4,19 @@ using Prism.Events;
 
 namespace Cameca.CustomAnalysis.Pca;
 
-internal class VoxelizationMenuFactory : AnalysisMenuFactoryBase
+internal class VoxelizationMenuFactory : ConditionalMenuFactoryBase
 {
-    public VoxelizationMenuFactory(IEventAggregator eventAggregator)
-        : base(eventAggregator)
+    public VoxelizationMenuFactory(IEventAggregator eventAggregator, INodeInfoProvider nodeInfoProvider) : base(eventAggregator, nodeInfoProvider)
     {
     }
 
     protected override INodeDisplayInfo DisplayInfo => VoxelizationAnalysis.DisplayInfo;
     protected override string NodeUniqueId => VoxelizationAnalysis.UniqueId;
     public override AnalysisMenuLocation Location { get; } = AnalysisMenuLocation.Analysis;
+
+    public override string[]? DisallowedTypes { get; } = new string[]
+    {
+        VoxelizationAnalysis.UniqueId,
+        KMeansAnalysis.UniqueId,
+    };
 }
